10f99eabe7b2cfd7f1795f24a2dc0d99b0e50be3
Rittick-Sinha <111774844+Rittick-Sinha@users.noreply.github.com>
2023-12-14 00:29:34 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/Rittick-Sinha?tab=repositories'
+      className='link footer__link'
+    >
+      Created By Rittick Sinha
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,61 @@
+const header = {
+  homepage: '',
+  title: '',
+}
+
+const about = {
+  name: 'Rittick Sinha',
+  role: 'Web Developer Engineer',
+  description:
+    "Hey, I'm Rittick Sinha, a web developer and designer exploring HTML, CSS, JavaScript, React JS, Node JS, and Express JS. I create sleek, user-friendly websites that blend tech and creativity for a captivating online experience.",
+  social: {
+    linkedin: 'https://www.linkedin.com/in/rittick-sinha-1a4a87166',
+    github: 'https://github.com/Rittick-Sinha',
+  },
+}
+
+const projects = [
+  {
+    name: 'Project 1',
+    description:
+      'A forthcoming webpage, crafted with React, dynamically adjusts to user interaction, offering a sneak peek while under development.',
+    stack: ['React', 'CSS'],
+    sourceCode: 'https://github.com/Rittick-Sinha/coming_soon',
+    livePreview: 'https://coming-soon-gilt.vercel.app/',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'A webpage crafted for event promotion and registration seamlessly adjusts to various screens, employing HTML, CSS, and JavaScript for comprehensive responsiveness.',
+    stack: ['HTML', 'Javascript', 'CSS'],
+    sourceCode: 'https://github.com/Rittick-Sinha/Astronomica',
+    livePreview: 'https://astronomica-eta.vercel.app/',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'I designed user-centric wireframes and prototypes for UI/UX, ensuring a seamless experience. Created website designs accommodating both light and dark modes, prioritizing user preferences',
+    stack: ['Figma'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://www.figma.com/file/rzfq7cvM0BBLRJi3pUflwE/UI%2FUX-Design?type=design&node-id=0%3A1&mode=design&t=3zTDwcjwSmwJDbps-1',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React Js',
+  'Redux',
+  'Material UI',
+  'Git',
+  'Node Js',
+  'Express Js',
+  'Ui/Ux',
+]
+
+const contact = {
+  email: 'sinharittick5@mail.com',
+}
+
+export { header, about, projects, skills, contact }
